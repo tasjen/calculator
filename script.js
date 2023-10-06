ac9b9fc3cb1f97bb5b62b315cf774e7e2b9d166c
tastas000 <TiiLg0o000@gmail.com>
2023-10-06 20:56:30 +0700

    Complete numbers, operators, and equal sign mechanism

--- script.js
+++ script.js
@@ -1,52 +1,92 @@
-let a = 0;
-let operator = '';
-let b = null;
+let x = 0;
+let operator = '+';
+let y = null;
 
-let isAC = true;
 let isOperatorPressed = false;
+let isEqualing = false;
 
-window.addEventListener('mousedown', (event) => {
-  event.preventDefault();
-  aMonitor.textContent = `a = ${a}`;
-  oMonitor.textContent = `o = ${operator}`;
-  bMonitor.textContent = `b = ${b}`;
-});
 const number = document.querySelectorAll('.number');
 const display = document.querySelector('#display');
+const operators = document.querySelectorAll('.op');
+
+
+
 number.forEach(each => {
   each.addEventListener('click', () => {
     if (isOperatorPressed || display.textContent === '0'){
       display.textContent = each.firstChild.textContent;
     }
-    else {
-      display.textContent += each.firstChild.textContent;
-    }
+    else display.textContent += each.firstChild.textContent;
+    
     isOperatorPressed = false;
   });
 })
 
-const operators = document.querySelectorAll('.operator');
+
 operators.forEach(each => {
   each.addEventListener('click', () => {
-    a = +display.textContent;
-    operator = each.textContent;
+    
+    if (isEqualing){
+      x = display.textContent;
+      isEqualing = false;
+      operator = each.textContent;
+      isOperatorPressed = true;
+      return;
+
+    }
+    if (isOperatorPressed){
+      operator = each.textContent;
+      return;
+    }
+    else {
+      y = display.textContent;
+      display.textContent = operate(x, operator, y);
+      operator = each.textContent;
+    }
+    x = display.textContent;
     isOperatorPressed = true;
-    if (each.textContent === '÷') operator = '/';
+
+
   })
 })
 
+
 const equal = document.querySelector('#equal');
 equal.addEventListener('click', () => {
-  display.textContent=''+operate(a, operator, b);
+  isOperatorPressed = false;
+  if (!isEqualing){
+    y = display.textContent;
+    display.textContent = operate(x, operator, y);
+    isEqualing = true;
+  }
+  else if (isOperatorPressed){
+    display.textContent = operate(display.textContent, operator, display.textContent);
+  }
+  else {
+    display.textContent = operate(display.textContent, operator, y);
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
-  if (isAC)
   display.textContent = '0';
+  operator = '+';
+  isOperatorPressed = false;
+  x='0';
+  y=null;
 });
-const AC = document.querySelector('#AC');
 
 
 
@@ -56,17 +96,6 @@ const AC = document.querySelector('#AC');
 
 
 
-
-const aMonitor = document.querySelector('#a');
-const oMonitor = document.querySelector('#operator');
-const bMonitor = document.querySelector('#b');
-
-
-window.addEventListener('load', () => {
-  aMonitor.textContent = `a = ${a}`;
-  oMonitor.textContent = `o = ${operator}`;
-  bMonitor.textContent = `b = ${b}`;
-});
 
 
 
@@ -113,12 +142,34 @@ function divide(a, b) {
 function operate(a, operator, b) {
   switch (operator) {
     case "+":
-      return add(a, b);
-    case "-":
-      return subtract(a, b);
-    case "*":
-      return multiply(a, b);
-    case "/":
-      return divide(a, b);
+      return Math.round(add(a, b) * 10 ** 8) / 10**8;
+    case "–":
+      return Math.round(subtract(a, b) * 10 ** 8) / 10**8;
+    case "x":
+      return Math.round(multiply(a, b) * 10 ** 8) / 10**8;
+    case "÷":
+      return Math.round(divide(a, b) * 10 ** 8) / 10**8;
   }
 }
+
+
+const aMonitor = document.querySelector('#a');
+const oMonitor = document.querySelector('#operator');
+const bMonitor = document.querySelector('#b');
+const eMonitor = document.querySelector('#e');
+const operatorPress = document.querySelector('#isOperatorPressed');
+window.addEventListener('click', () => {
+  aMonitor.textContent = `x = ${x}`;
+  oMonitor.textContent = `o = ${operator}`;
+  bMonitor.textContent = `y = ${y}`;
+  operatorPress.textContent = `isOperatorPressed = ${isOperatorPressed}`;
+  eMonitor.textContent = `isEqualing = ${isEqualing}`;
+
+});
+window.addEventListener('load', () => {
+  aMonitor.textContent = `x = ${x}`;
+  oMonitor.textContent = `o = ${operator}`;
+  bMonitor.textContent = `y = ${y}`;
+  operatorPress.textContent = `isOperatorPressed = ${isOperatorPressed}`;
+  eMonitor.textContent = `isEqualing = ${isEqualing}`;
+});
